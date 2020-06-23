30baa6980d6d518a0de6e56b046e72e70c6f6b83
TadasBalsys <balsys.tadas@gmail.com>
2020-06-23 17:06:24 +0300

    Refactored App component to functional component and used useState hook.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Person from './Person/person';
 import './App.css';
@@ -6,7 +6,6 @@ import './App.css';
 interface PersonProps {
   name: string;
   age: number;
-  children: React.ReactNode;
 }
 
 interface PersonData {
@@ -14,37 +13,55 @@ interface PersonData {
   age: number;
 }
 
-interface PersonState {
-  people: PersonData[];
-}
+// interface PersonState {
+//   people: PersonData[];
+// }
 
-class App extends React.Component<{}, PersonState> {
-  constructor(props: PersonData) {
-    super(props);
+const initialState: PersonData[] = [
+  { name: 'Tadas', age: 33 },
+  { name: 'Aina', age: 30 },
+];
 
-    this.state = {
-      people: [
-        {
-          name: 'Tadas',
-          age: 33,
-        },
-        {
-          name: 'Aina',
-          age: 30,
-        },
-      ],
-    };
-  }
-  render() {
-    return (
-      <div className='App'>
-        <h1>Hello World</h1>
-        {this.state.people.map((person: PersonData, i: number) => (
-          <Person key={i} {...person} />
-        ))}
-      </div>
-    );
-  }
-}
+const App = (): JSX.Element => {
+  const [state, setState] = useState<PersonData[]>(initialState);
+
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      {state.map((person: PersonData, i: number) => (
+        <Person key={i} {...person} />
+      ))}
+    </div>
+  );
+};
+
+// class App extends React.Component<{}, PersonState> {
+// constructor(props: PersonData) {
+//     super(props);
+
+//     this.state = {
+//       people: [
+//         {
+//           name: 'Tadas',
+//           age: 33,
+//         },
+//         {
+//           name: 'Aina',
+//           age: 30,
+//         },
+//       ],
+//     };
+//   }
+//   render() {
+//     return (
+//       <div className='App'>
+//         <h1>Hello World</h1>
+//         {this.state.people.map((person: PersonData, i: number) => (
+//           <Person key={i} {...person} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
